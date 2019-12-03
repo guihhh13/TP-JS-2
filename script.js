97dcce8fac980d93ae8f09da81cd3fbcd32c8b04
glyve99 <guilhermemolyveira@hotmail.com>
2019-12-03 11:37:41 -0300

    Tentando fazer criação de div a partir de forms funcionar

--- script.js
+++ script.js
@@ -11,4 +11,14 @@ function fechaModal(modalId){
 const copag = document.querySelector('.copag');
     copag.addEventListener('click', function(){
         iniciaModal('NewTask-p');
-    });
+});
+
+const f = document.getElementById('.my-form');
+f.addEventListener('submit', function(){
+    let name = document.querySelector('input[name="nome"]').value;
+    let dc = document.querySelector('input[name="Descricao"]').value;
+    d = document.createElement('div');
+    d.appendChild(document.createTextNode(name));
+    d.appendChild(document.createTextNode(dc));
+    document.getElementById('.toDo-e').appendChild(d);
+})
