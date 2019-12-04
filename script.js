e83ec9280e68feb6ac56d6c8fa662189a2b16cc3
glyve99 <guilhermemolyveira@hotmail.com>
2019-12-04 15:29:59 -0300

    adição de tarefas funcionando

--- script.js
+++ script.js
@@ -10,15 +10,31 @@ function fechaModal(modalId){
 
 const copag = document.querySelector('.copag');
     copag.addEventListener('click', function(){
+        document.getElementById('NewTask-p').style.display='block';
         iniciaModal('NewTask-p');
 });
 
-const f = document.getElementById('.my-form');
-f.addEventListener('submit', function(){
-    let name = document.querySelector('input[name="nome"]').value;
-    let dc = document.querySelector('input[name="Descricao"]').value;
-    d = document.createElement('div');
-    d.appendChild(document.createTextNode(name));
-    d.appendChild(document.createTextNode(dc));
-    document.getElementById('.toDo-e').appendChild(d);
-})
+function addDiv(){
+    
+    const nome = document.querySelector('.nome').value;
+    const descricao = document.querySelector('.descricao').value;
+    const div = document.createElement('div');
+    const divp = document.createElement('div');
+    const divh6 = document.createElement('div');
+    div.setAttribute('class', 'todos');
+    divp.setAttribute('class', 'todos-nome');
+    divh6.setAttribute('class', 'todos-descricao');
+    const p = document.createElement('p');
+    const h6 = document.createElement('h6');
+    p.setAttribute('class', 'todos-p');
+    h6.setAttribute('class', 'todos-h6');
+    p.innerText = nome;
+    h6.innerText = descricao;
+    divp.appendChild(p);
+    divh6.appendChild(h6);
+    div.appendChild(divp);
+    div.appendChild(divh6);
+    document.querySelector('.toDo').appendChild(div);
+    document.getElementById('NewTask-p').style.display='none';
+
+}
